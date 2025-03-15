23202413b1688c98742d9e2b3e345f0327ca93f8
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-03-15 20:00:01 +0000

    feat!: break MimeType into main and subtype
    Changes MimeType to be a struct containing the 'group' (E.G application,
    video, audio) and the subtype (E.G JSON, text, DOCX). Retains the
    original string as well

--- src/mime.rs
+++ src/mime.rs
@@ -1,6 +1,24 @@
 use std::str::FromStr;
 
-pub enum MimeType {
+#[derive(Debug, PartialEq)]
+pub struct MimeType {
+    main_type: MainMimeType,
+    sub_type: SubMimeType,
+    original: String,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum MainMimeType {
+    Application,
+    Audio,
+    Font,
+    Image,
+    Text,
+    Video,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum SubMimeType {
     AAC,
     ABW,
     APNG,
@@ -80,85 +98,103 @@ impl FromStr for MimeType {
     type Err = &'static str;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "audio/aac" => Ok(MimeType::AAC),
-            "application/x-abiword" => Ok(MimeType::ABW),
-            "image/apng" => Ok(MimeType::APNG),
-            "application/x-freearc" => Ok(MimeType::ARC),
-            "image/avif" => Ok(MimeType::AVIF),
-            "video/x-msvideo" => Ok(MimeType::AVI),
-            "application/vnd.amazon.ebook" => Ok(MimeType::AZW),
-            "application/octet-stream" => Ok(MimeType::BIN),
-            "image/bmp" => Ok(MimeType::BMP),
-            "application/x-bzip" => Ok(MimeType::BZ),
-            "application/x-bzip2" => Ok(MimeType::BZ2),
-            "application/x-cdf" => Ok(MimeType::CDA),
-            "application/x-csh" => Ok(MimeType::CSH),
-            "text/css" => Ok(MimeType::CSS),
-            "text/csv" => Ok(MimeType::CSV),
-            "application/msword" => Ok(MimeType::DOC),
+        let (main_type, sub_type) = match s {
+            "audio/aac" => (MainMimeType::Audio, SubMimeType::AAC),
+            "application/x-abiword" => (MainMimeType::Application, SubMimeType::ABW),
+            "image/apng" => (MainMimeType::Image, SubMimeType::APNG),
+            "application/x-freearc" => (MainMimeType::Application, SubMimeType::ARC),
+            "image/avif" => (MainMimeType::Image, SubMimeType::AVIF),
+            "video/x-msvideo" => (MainMimeType::Video, SubMimeType::AVI),
+            "application/vnd.amazon.ebook" => (MainMimeType::Application, SubMimeType::AZW),
+            "application/octet-stream" => (MainMimeType::Application, SubMimeType::BIN),
+            "image/bmp" => (MainMimeType::Image, SubMimeType::BMP),
+            "application/x-bzip" => (MainMimeType::Application, SubMimeType::BZ),
+            "application/x-bzip2" => (MainMimeType::Application, SubMimeType::BZ2),
+            "application/x-cdf" => (MainMimeType::Application, SubMimeType::CDA),
+            "application/x-csh" => (MainMimeType::Application, SubMimeType::CSH),
+            "text/css" => (MainMimeType::Text, SubMimeType::CSS),
+            "text/csv" => (MainMimeType::Text, SubMimeType::CSV),
+            "application/msword" => (MainMimeType::Application, SubMimeType::DOC),
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document" => {
-                Ok(MimeType::DOCX)
+                (MainMimeType::Application, SubMimeType::DOCX)
             }
-            "application/vnd.ms-fontobject" => Ok(MimeType::EOT),
-            "application/epub+zip" => Ok(MimeType::EPUB),
-            "application/gzip" | ".gz" | "application/x-gzip" => Ok(MimeType::GZ),
-            "image/gif" => Ok(MimeType::GIF),
-            "text/html" => Ok(MimeType::HTM),
-            "image/vnd.microsoft.icon" => Ok(MimeType::ICO),
-            "text/calendar" => Ok(MimeType::ICS),
-            "application/java-archive" => Ok(MimeType::JAR),
-            "image/jpeg" => Ok(MimeType::JPEG),
-            "text/javascript" => Ok(MimeType::JS),
-            "application/json" => Ok(MimeType::JSON),
-            "application/ld+json" => Ok(MimeType::JSONLD),
-            "audio/midi" | "audio/x-midi" => Ok(MimeType::MID),
-            "audio/mpeg" => Ok(MimeType::MP3),
-            "video/mp4" => Ok(MimeType::MP4),
-            "video/mpeg" => Ok(MimeType::MPEG),
-            "application/vnd.apple.installer+xml" => Ok(MimeType::MPKG),
-            "application/vnd.oasis.opendocument.presentation" => Ok(MimeType::ODP),
-            "application/vnd.oasis.opendocument.spreadsheet" => Ok(MimeType::ODS),
-            "application/vnd.oasis.opendocument.text" => Ok(MimeType::ODT),
-            "audio/ogg" => Ok(MimeType::OGA),
-            "video/ogg" => Ok(MimeType::OGV),
-            "application/ogg" => Ok(MimeType::OGX),
-            "font/otf" => Ok(MimeType::OTF),
-            "image/png" => Ok(MimeType::PNG),
-            "application/pdf" => Ok(MimeType::PDF),
-            "application/x-httpd-php" => Ok(MimeType::PHP),
-            "application/vnd.ms-powerpoint" => Ok(MimeType::PPT),
+            "application/vnd.ms-fontobject" => (MainMimeType::Application, SubMimeType::EOT),
+            "application/epub+zip" => (MainMimeType::Application, SubMimeType::EPUB),
+            "application/gzip" | ".gz" | "application/x-gzip" => {
+                (MainMimeType::Application, SubMimeType::GZ)
+            }
+            "image/gif" => (MainMimeType::Image, SubMimeType::GIF),
+            "text/html" => (MainMimeType::Text, SubMimeType::HTM),
+            "image/vnd.microsoft.icon" => (MainMimeType::Image, SubMimeType::ICO),
+            "text/calendar" => (MainMimeType::Text, SubMimeType::ICS),
+            "application/java-archive" => (MainMimeType::Application, SubMimeType::JAR),
+            "image/jpeg" => (MainMimeType::Image, SubMimeType::JPEG),
+            "text/javascript" => (MainMimeType::Text, SubMimeType::JS),
+            "application/json" => (MainMimeType::Application, SubMimeType::JSON),
+            "application/ld+json" => (MainMimeType::Application, SubMimeType::JSONLD),
+            "audio/midi" | "audio/x-midi" => (MainMimeType::Audio, SubMimeType::MID),
+            "audio/mpeg" => (MainMimeType::Audio, SubMimeType::MP3),
+            "video/mp4" => (MainMimeType::Video, SubMimeType::MP4),
+            "video/mpeg" => (MainMimeType::Video, SubMimeType::MPEG),
+            "application/vnd.apple.installer+xml" => (MainMimeType::Application, SubMimeType::MPKG),
+            "application/vnd.oasis.opendocument.presentation" => {
+                (MainMimeType::Application, SubMimeType::ODP)
+            }
+            "application/vnd.oasis.opendocument.spreadsheet" => {
+                (MainMimeType::Application, SubMimeType::ODS)
+            }
+            "application/vnd.oasis.opendocument.text" => {
+                (MainMimeType::Application, SubMimeType::ODT)
+            }
+            "audio/ogg" => (MainMimeType::Audio, SubMimeType::OGA),
+            "video/ogg" => (MainMimeType::Video, SubMimeType::OGV),
+            "application/ogg" => (MainMimeType::Application, SubMimeType::OGX),
+            "font/otf" => (MainMimeType::Font, SubMimeType::OTF),
+            "image/png" => (MainMimeType::Image, SubMimeType::PNG),
+            "application/pdf" => (MainMimeType::Application, SubMimeType::PDF),
+            "application/x-httpd-php" => (MainMimeType::Application, SubMimeType::PHP),
+            "application/vnd.ms-powerpoint" => (MainMimeType::Application, SubMimeType::PPT),
             "application/vnd.openxmlformats-officedocument.presentationml.presentation" => {
-                Ok(MimeType::PPTX)
+                (MainMimeType::Application, SubMimeType::PPTX)
             }
-            "application/vnd.rar" => Ok(MimeType::RAR),
-            "application/rtf" => Ok(MimeType::RTF),
-            "application/x-sh" => Ok(MimeType::SH),
-            "image/svg+xml" => Ok(MimeType::SVG),
-            "application/x-tar" => Ok(MimeType::TAR),
-            "image/tiff" => Ok(MimeType::TIF),
-            "video/mp2t" => Ok(MimeType::TS),
-            "font/ttf" => Ok(MimeType::TTF),
-            "text/plain" => Ok(MimeType::TXT),
-            "application/vnd.visio" => Ok(MimeType::VSD),
-            "audio/wav" => Ok(MimeType::WAV),
-            "audio/webm" => Ok(MimeType::WEBA),
-            "video/webm" => Ok(MimeType::WEBM),
-            "image/webp" => Ok(MimeType::WEBP),
-            "font/woff" => Ok(MimeType::WOFF),
-            "font/woff2" => Ok(MimeType::WOFF2),
-            "application/xhtml+xml" => Ok(MimeType::XHTML),
-            "application/vnd.ms-excel" => Ok(MimeType::XLS),
+            "application/vnd.rar" => (MainMimeType::Application, SubMimeType::RAR),
+            "application/rtf" => (MainMimeType::Application, SubMimeType::RTF),
+            "application/x-sh" => (MainMimeType::Application, SubMimeType::SH),
+            "image/svg+xml" => (MainMimeType::Image, SubMimeType::SVG),
+            "application/x-tar" => (MainMimeType::Application, SubMimeType::TAR),
+            "image/tiff" => (MainMimeType::Image, SubMimeType::TIF),
+            "video/mp2t" => (MainMimeType::Video, SubMimeType::TS),
+            "font/ttf" => (MainMimeType::Font, SubMimeType::TTF),
+            "text/plain" => (MainMimeType::Text, SubMimeType::TXT),
+            "application/vnd.visio" => (MainMimeType::Application, SubMimeType::VSD),
+            "audio/wav" => (MainMimeType::Audio, SubMimeType::WAV),
+            "audio/webm" => (MainMimeType::Audio, SubMimeType::WEBA),
+            "video/webm" => (MainMimeType::Video, SubMimeType::WEBM),
+            "image/webp" => (MainMimeType::Image, SubMimeType::WEBP),
+            "font/woff" => (MainMimeType::Font, SubMimeType::WOFF),
+            "font/woff2" => (MainMimeType::Font, SubMimeType::WOFF2),
+            "application/xhtml+xml" => (MainMimeType::Application, SubMimeType::XHTML),
+            "application/vnd.ms-excel" => (MainMimeType::Application, SubMimeType::XLS),
             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" => {
-                Ok(MimeType::XLSX)
+                (MainMimeType::Application, SubMimeType::XLSX)
             }
-            "application/xml" | "text/xml" | ".xml" | "application/atom+xml" => Ok(MimeType::XML),
-            "application/vnd.mozilla.xul+xml" => Ok(MimeType::XUL),
-            "application/zip" | ".zip" | "application/x-zip-compressed" => Ok(MimeType::ZIP),
-            "video/3gpp" | "audio/3gpp" => Ok(MimeType::_3GP),
-            "video/3gpp2" | "audio/3gpp2" => Ok(MimeType::_3G2),
-            "application/x-7z-compressed" => Ok(MimeType::_7Z),
-            _ => Err("Not a valid MIME type"),
-        }
+            "application/xml" | "text/xml" | ".xml" | "application/atom+xml" => {
+                (MainMimeType::Application, SubMimeType::XML)
+            }
+            "application/vnd.mozilla.xul+xml" => (MainMimeType::Application, SubMimeType::XUL),
+            "application/zip" | ".zip" | "application/x-zip-compressed" => {
+                (MainMimeType::Application, SubMimeType::ZIP)
+            }
+            "video/3gpp" | "audio/3gpp" => (MainMimeType::Video, SubMimeType::_3GP),
+            "video/3gpp2" | "audio/3gpp2" => (MainMimeType::Video, SubMimeType::_3G2),
+            "application/x-7z-compressed" => (MainMimeType::Application, SubMimeType::_7Z),
+            _ => return Err("Not a valid MIME type"),
+        };
+
+        Ok(MimeType {
+            main_type,
+            sub_type,
+            original: s.to_string(),
+        })
     }
 }
