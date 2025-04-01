0f36fa7b020d6608ec5b6dbbb4f0b538ef387dce
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-04-01 23:34:47 +0100

    docs: document weird function type

--- src/server/handlers.rs
+++ src/server/handlers.rs
@@ -6,6 +6,7 @@ use crate::server::response::Response;
 pub struct Handler<'a> {
     path: Path,
     method: HTTPMethod,
+    // See https://stackoverflow.com/questions/27831944/how-do-i-store-a-closure-in-a-struct-in-rust/27832320#27832320
     pub callback: &'a mut dyn FnMut(Request) -> Response,
 }
 
