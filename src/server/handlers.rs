8810c8c7788983863cc05c0a41f93dd10cd54c27
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-04-07 01:59:48 +0100

    feat: implement add and get for HandlerRegistry
    Uses a composite key of format "{HTTPMethod}[##]{HandlerPath}" to
    discriminate between different HTTP verbs for the same paths

--- src/server/handlers.rs
+++ src/server/handlers.rs
@@ -1,7 +1,12 @@
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
-use crate::request::{HTTPMethod, Path, Request};
+use crate::request::{HTTPMethod, Request};
 use crate::server::response::Response;
+
+static KEY_DELIMITER: &str = "[##]";
+
+type HandlerCallback<'a> = &'a mut dyn FnMut(Request) -> Response;
 
 #[derive(PartialEq, Debug)]
 pub struct HandlerPath(String);
@@ -15,29 +20,79 @@ impl HandlerPath {
         Self(path.strip_suffix('/').unwrap_or(path).to_string())
     }
 }
+
 pub struct Handler<'a> {
     path: HandlerPath,
     method: HTTPMethod,
     // See https://stackoverflow.com/questions/27831944/how-do-i-store-a-closure-in-a-struct-in-rust/27832320#27832320
-    pub callback: &'a mut dyn FnMut(Request) -> Response,
+    pub callback: HandlerCallback<'a>,
+}
+
+/**
+   A composite key from a handler. This is necessary because paths can be reused for
+   different HTTP verbs
+*/
+#[derive(Hash, PartialEq, Eq, Clone)]
+struct HandlerRegistryKey(String);
+
+impl<'a> From<&Handler<'a>> for HandlerRegistryKey {
+    fn from(handler: &Handler<'a>) -> Self {
+        Self(format!(
+            "{0}{KEY_DELIMITER}{1}",
+            handler.method, handler.path.0
+        ))
+    }
+}
+
+impl From<(HTTPMethod, String)> for HandlerRegistryKey {
+    fn from((method, path): (HTTPMethod, String)) -> Self {
+        Self(format!("{0}{KEY_DELIMITER}{1}", method, path))
+    }
 }
 
 #[derive(Default)]
 pub struct HandlerRegistry<'a> {
     // TODO: figure out how to efficiently discriminate between HTTP methods
-    handlers: HashMap<String, Handler<'a>>,
+    handlers: HashMap<HandlerRegistryKey, Handler<'a>>,
 }
 
 impl<'a> Handler<'a> {
-    pub fn new(
-        path: Path,
-        method: HTTPMethod,
-        callback: &'a mut dyn FnMut(Request) -> Response,
-    ) -> Self {
+    pub fn new(path: &str, method: HTTPMethod, callback: HandlerCallback<'a>) -> Self {
         Self {
             path: HandlerPath(path.to_string()),
             method,
             callback,
         }
+    }
+
+    pub fn call(&mut self, request: Request) -> Response {
+        (self.callback)(request)
+    }
+}
+
+struct DuplicateKey(HandlerRegistryKey);
+
+impl<'a> HandlerRegistry<'a> {
+    pub fn new(handlers: Vec<Handler<'a>>) -> Self {
+        let mut registry = HashMap::new();
+        handlers.into_iter().for_each(|h| {
+            registry.entry(HandlerRegistryKey::from(&h)).or_insert(h);
+        });
+        HandlerRegistry { handlers: registry }
+    }
+
+    pub fn add(&mut self, handler: Handler<'a>) -> Result<(), DuplicateKey> {
+        let key = HandlerRegistryKey::from(&handler);
+
+        if let Entry::Vacant(e) = self.handlers.entry(key.clone()) {
+            e.insert(handler);
+            Ok(())
+        } else {
+            Err(DuplicateKey(key))
+        }
+    }
+
+    pub fn get(&self, method: HTTPMethod, path: String) -> Option<&Handler<'a>> {
+        self.handlers.get(&HandlerRegistryKey::from((method, path)))
     }
 }
