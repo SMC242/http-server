29cb5d285fb65d65f0c20364eb67d5e91e8967e5
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-04-07 01:52:05 +0100

    fix!: narrow Handler.path's type to relative paths
    Handlers only make sense as relative to the server as this is not a
    proxy server

--- src/server/handlers.rs
+++ src/server/handlers.rs
@@ -3,8 +3,20 @@ use std::collections::HashMap;
 use crate::request::{HTTPMethod, Path, Request};
 use crate::server::response::Response;
 
+#[derive(PartialEq, Debug)]
+pub struct HandlerPath(String);
+
+/// A relative path to match against
+impl HandlerPath {
+    pub fn new(path: &str) -> Self {
+        if !path.starts_with('/') {
+            panic!("Invalid path {path}. Must be a relative path")
+        }
+        Self(path.strip_suffix('/').unwrap_or(path).to_string())
+    }
+}
 pub struct Handler<'a> {
-    path: Path,
+    path: HandlerPath,
     method: HTTPMethod,
     // See https://stackoverflow.com/questions/27831944/how-do-i-store-a-closure-in-a-struct-in-rust/27832320#27832320
     pub callback: &'a mut dyn FnMut(Request) -> Response,
@@ -23,7 +35,7 @@ impl<'a> Handler<'a> {
         callback: &'a mut dyn FnMut(Request) -> Response,
     ) -> Self {
         Self {
-            path,
+            path: HandlerPath(path.to_string()),
             method,
             callback,
         }
