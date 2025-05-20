448eae548a7bca57ab7da491fd0812187d976bcc
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-05-20 02:15:31 +0100

    refactor: create HTTPListener struct
    Moves the behaviour contained in main's handle_connection into its own
    struct, splitting up the stages into functions. Also fixes a logical
    error where the DogStore would be reinitialised for each request

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,29 @@
-use log::{debug, info};
-use request::{HTTPHeaders, HTTPVersion};
-use server::handlers::{Handler, HandlerRegistry};
-use server::response::{Response, ResponseStatus};
-use std::io::{BufRead, BufReader, Error as IoError, ErrorKind, Read, Write};
-use std::net::{Ipv4Addr, TcpStream};
+use log::info;
+use server::handlers::HandlerRegistry;
+use server::listener::{self, ListenerConfig};
+use std::net::{IpAddr, Ipv4Addr};
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
 
 mod dog_crud_example;
-use dog_crud_example as dogstore;
+use dog_crud_example::{self as dogstore, DogStoreGetHandler, DogStorePostHandler};
 mod mime;
 mod request;
 mod server;
 
-static IP: Ipv4Addr = Ipv4Addr::LOCALHOST;
+static IP: IpAddr = IpAddr::V4(Ipv4Addr::LOCALHOST);
 // TODO: increment if port is unavailable. Will require this to not be static
 static PORT: u16 = 8080;
-static TIMEOUT: Option<Duration> = Some(Duration::new(10, 0));
-static DEFAULT_RESPONSE: &str = "HTTP/1.1 200 OK\r\n";
-static CARRIAGE_RETURN: &str = "\r\n";
-
-fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
-    let client_ip: String = stream
-        .peer_addr()
-        .map(|addr| addr.to_string())
-        .unwrap_or("IP address unknown".to_string());
-    info!(target: "listener", "Connection received from {client_ip}");
-
-    info!(target: "listener", "Configuring connection for {client_ip}");
-    stream.set_read_timeout(TIMEOUT)?;
-    stream.set_write_timeout(TIMEOUT)?;
-
-    let mut request_content = String::new();
-    // Read until end of request head (empty line).
-    // NOTE: further reading will be required to get the request body
-    let mut reader = stream.try_clone().map(BufReader::new)?;
-    // This ultimately does 2 passes through the connection :( Would it be possible to cut out
-    // the first pass? The main reason for it is to unwrap each line
-    for line in reader.by_ref().lines() {
-        let mut unwrapped = line?;
-        if unwrapped.is_empty() {
-            break;
-        } else {
-            let n_bytes = unwrapped.len();
-            info!(target: "listener", "Read {n_bytes} from {client_ip}");
-            unwrapped.push_str(CARRIAGE_RETURN);
-            request_content += &unwrapped;
-        }
-    }
-
-    info!(target: "listener", "Parsing message from {client_ip} as HTTP request");
-    // This iterator will be adavanced to the request body
-    //let readerfn: FnMut(usize) -> Box<[u8]> = |size| reader.read(&mut Box::new([0u8; size]));
-    let req_lines = &mut request_content.lines();
-    let req_head = request::http1_1::parse_req_head(req_lines).map_err(|err| {
-        info!(target: "listener", "Failed to parse request from {client_ip} due to the following error: {err}");
-        IoError::new(
-            ErrorKind::InvalidData,
-            "Could not parse message as HTTP request",
-        )
-    })?;
-    info!(target: "listener", "Request received from {client_ip}: {req_head:?}");
-
-    let request = request::Request::new(req_head, reader);
-
-    let dog_store = Arc::new(Mutex::new(dogstore::DogStore::default()));
-    let handlers: Vec<Arc<dyn Handler>> = vec![
-        Arc::new(dogstore::DogStoreGetHandler::new(dog_store.clone())),
-        Arc::new(dogstore::DogStorePostHandler::new(dog_store.clone())),
-    ];
-
-    let handler_registry = HandlerRegistry::new(handlers);
-
-    let response = if let Ok(request_path) = request.head.path.clone().try_into() {
-        match handler_registry.get(request.head.method, request_path) {
-            Some(handler) => handler.on_request(&request),
-            None => Response::new(
-                HTTPVersion::V1_1,
-                ResponseStatus::NotFound,
-                HTTPHeaders::default(),
-                "No matching handler found".to_string(),
-            ),
-        }
-    } else {
-        Response::new(
-            HTTPVersion::V1_1,
-            ResponseStatus::BadRequest,
-            HTTPHeaders::default(),
-            "Malformed URL path".to_string(),
-        )
-    };
-    stream.write_all(response.to_string().as_bytes())
-}
 
 fn main() -> std::io::Result<()> {
     env_logger::init();
+
+    info!(target: "listener", "Initialising handlers");
+    let dog_store = Arc::new(Mutex::new(dogstore::DogStore::default()));
+    let registry = HandlerRegistry::new(vec![
+        Arc::new(DogStoreGetHandler::new(dog_store.clone())),
+        Arc::new(DogStorePostHandler::new(dog_store.clone())),
+    ]);
+
     info!(target: "listener", "Starting server on {IP}:{PORT}");
-    server::listener::listen(IP, PORT, handle_connection)
+    listener::HTTPListener::new(IP, PORT, registry, ListenerConfig::default()).listen()
 }
--- src/server/listener.rs
+++ src/server/listener.rs
@@ -1,6 +1,23 @@
-use std::net::{Ipv4Addr, TcpListener, TcpStream};
+use crate::request::{self, http1_1, HTTPHeaders, HTTPVersion};
+use std::{
+    io::{BufRead, BufReader, Error as IoError, ErrorKind, Read, Write},
+    net::{IpAddr, TcpListener, TcpStream},
+};
 
-pub fn listen<E, F>(ip: Ipv4Addr, port: u16, mut on_stream: F) -> std::io::Result<()>
+use log::info;
+
+use crate::request::{Request, RequestParseError};
+
+use super::{
+    handlers::HandlerRegistry,
+    response::{Response, ResponseStatus},
+};
+
+static CARRIAGE_RETURN: &str = "\r\n";
+
+/// A low-level function for receiving and operating on TCP connections.
+/// Use `Listener` for a higher level interface
+pub fn listen<E, F>(ip: IpAddr, port: u16, mut on_stream: F) -> std::io::Result<()>
 where
     F: FnMut(TcpStream) -> Result<(), E>,
     E: std::fmt::Debug,
@@ -11,4 +28,133 @@ where
             .inspect_err(|err| println!("Error occurred in on_stream: {0:?}", err));
     }
     Ok(())
+}
+
+#[derive(Debug)]
+pub struct ListenerConfig {
+    timeout: Option<std::time::Duration>,
+}
+
+impl Default for ListenerConfig {
+    fn default() -> Self {
+        Self {
+            timeout: Some(std::time::Duration::new(10, 0)),
+        }
+    }
+}
+
+/// Parses incoming HTTP messages from TCP connections using
+/// the given parse function before dispatching the request to handlers.
+/// Will support middleware in the future
+pub struct HTTPListener {
+    ip: IpAddr,
+    port: u16,
+    handler_registry: HandlerRegistry,
+    config: ListenerConfig,
+}
+
+impl HTTPListener {
+    pub fn new(
+        ip: IpAddr,
+        port: u16,
+        handler_registry: HandlerRegistry,
+        config: ListenerConfig,
+    ) -> Self {
+        Self {
+            ip,
+            port,
+            handler_registry,
+            config,
+        }
+    }
+
+    pub fn listen(&self) -> std::io::Result<()> {
+        listen(self.ip, self.port, |mut conn| {
+            self.handle_connection(&mut conn)
+        })
+    }
+
+    fn handle_connection(&self, stream: &mut TcpStream) -> Result<(), IoError> {
+        let client_ip: String = stream
+            .peer_addr()
+            .map(|addr| addr.to_string())
+            .unwrap_or("IP address unknown".to_string());
+        info!(target: "listener", "Connection received from {client_ip}");
+
+        info!(target: "listener", "Configuring connection for {client_ip}");
+        self.configure_connection(stream);
+
+        let (request_content, reader) = self.read_message(stream)?;
+        info!(target: "listener", "Parsing message from {client_ip} as HTTP request");
+
+        let request_head = self.parse_message(request_content).map_err(|err| {
+        info!(target: "listener", "Failed to parse request from {client_ip} due to the following error: {err}");
+        IoError::new(
+            ErrorKind::InvalidData,
+            "Could not parse message as HTTP request",
+        )
+    })?;
+        info!(target: "listener", "Request received from {client_ip}: {request_head:?}");
+
+        let request = request::Request::new(request_head, reader);
+
+        let response = self.dispatch(request);
+        stream.write_all(response.to_string().as_bytes())
+    }
+
+    fn configure_connection(&self, conn: &TcpStream) -> Result<(), IoError> {
+        conn.set_read_timeout(self.config.timeout)?;
+        conn.set_write_timeout(self.config.timeout)?;
+        Ok(())
+    }
+
+    fn read_message(&self, stream: &TcpStream) -> Result<(String, BufReader<TcpStream>), IoError> {
+        let mut request_content = String::new();
+        // Read until end of request head (empty line).
+        // NOTE: further reading will be required to get the request body
+        let mut reader = stream.try_clone().map(BufReader::new)?;
+        // This ultimately does 2 passes through the connection :( Would it be possible to cut out
+        // the first pass? The main reason for it is to unwrap each line
+        for line in reader.by_ref().lines() {
+            let mut unwrapped = line?;
+            if unwrapped.is_empty() {
+                break;
+            } else {
+                unwrapped.push_str(CARRIAGE_RETURN);
+                request_content += &unwrapped;
+            }
+        }
+
+        Ok((request_content, reader))
+    }
+
+    fn parse_message(
+        &self,
+        message: String,
+    ) -> Result<crate::request::RequestHead, RequestParseError> {
+        // This iterator will be adavanced to the request body
+        let req_lines = &mut message.lines();
+        http1_1::parse_req_head(req_lines)
+    }
+
+    fn dispatch(&self, request: Request) -> Response {
+        if let Ok(request_path) = request.head.path.clone().try_into() {
+            match self.handler_registry.get(request.head.method, request_path) {
+                Some(handler) => handler.on_request(&request),
+                None => Response::new(
+                    HTTPVersion::V1_1,
+                    super::response::ResponseStatus::NotFound,
+                    HTTPHeaders::default(),
+                    "No matching handler found".to_string(),
+                ),
+            }
+        } else {
+            Response::new(
+                HTTPVersion::V1_1,
+                ResponseStatus::BadRequest,
+                HTTPHeaders::default(),
+                "Malformed URL path".to_string(),
+            )
+        }
+    }
 }
